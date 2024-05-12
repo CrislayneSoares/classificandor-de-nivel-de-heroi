339cfccef9e74fa38520aea72be0e2faf3258976
CrislayneSoares <crissoaresr@hotmail.com>
2024-05-12 15:37:45 -0300

    desafio 2

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+// Desafio 1 
+// Definir a patente do jogador a partir do XP ganho.
 let Patente
 let XP
 let nome = "Cris"
@@ -28,5 +30,55 @@ if (XP <= 1000) {
     Patente = "Radiante"
 }
 
-
+console.log("Desafio 1")
 console.log("O Herói de "+ nome +" está no nível "+ Patente)
+console.log("-----------------------------------------------------------------")
+
+// Desafio 2
+// Definir a patente do jogador a partir da quantidade de vitorias e derrotas rankeadas do jogador
+
+let vitorias = 0
+let derrotas = 0
+let saldoVitorias = 0
+let nivel = ""
+
+function gerarVitorias(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    vitorias = parseInt(Math.random() * (max - min) + min)
+}
+
+function gerarDerrotas(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    derrotas = parseInt(Math.random() * (max - min) + min)
+}
+
+gerarVitorias(1, 200)
+
+gerarDerrotas(1, 50)
+
+function rankeadas(vitorias, derrotas){
+    saldoVitorias = vitorias - derrotas
+    if (saldoVitorias <= 10 ) {
+        nivel = "Ferro"
+    } else if(saldoVitorias > 10 && saldoVitorias <= 20){
+        nivel = "Bronze"
+    } else if (saldoVitorias > 20 && saldoVitorias <= 50) {
+        nivel = "Prata"
+    } else if (saldoVitorias > 50 && saldoVitorias <= 80) {
+        nivel = "Ouro"
+    } else if (saldoVitorias > 80 && saldoVitorias <= 90) {
+        nivel = "Diamante"
+    } else if (saldoVitorias > 90 && saldoVitorias <= 100){
+        nivel = "Lendário"
+    } else if (saldoVitorias >= 101) {
+        nivel = "Imortal"
+    }
+}
+
+rankeadas(vitorias, derrotas)
+
+console.log("Desafio 2")
+console.log("O Herói tem de saldo de " + saldoVitorias + " está no nível de " + nivel + ".")
+console.log("-----------------------------------------------------------------")
